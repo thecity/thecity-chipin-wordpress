79a08676ba3410e3fb1362b626224583ca3d7c76
weshays <weshays@gbdev.com>
2013-07-26 08:15:18 -0700

    Updated widget area to include needed fields

--- thecity_chipin.php
+++ thecity_chipin.php
@@ -1,54 +1,69 @@
 <?php
 /*
-Plugin Name: The City Login Widget
-Plugin URI: http://developer.onthecity.org/thecity-plugins/wordpress-login/
-Description: A WordPress plugin that allows users to login directly to The City from your WordPress website.
+Plugin Name: The City Chipin Widget
+Plugin URI: http://developer.onthecity.org/thecity-plugins/wordpress-chipin/
+Description: A WordPress plugin that uses The City Admin API (api.onthecity.org) so that donations can be segmented for a campaign registry.
 Author: Wes Hays
-Version: 0.3
+Version: 0.1
 Author URI: http://www.OnTheCity.org
 */
 
-include_once 'thecity_login_scripts.php';
+include_once 'thecity_chipin_scripts.php';
 
 
-class The_City_Login_Widget extends WP_Widget {
+class The_City_Chipin_Widget extends WP_Widget {
   
   function __construct() {
-    $widget_ops = array('classname' => 'the_city_login_widget', 
-                        'description' => 'A WordPress plugin that allows users to login directly to The City from your WordPress website.' );
-    $this->WP_Widget('the-city-login-widget', 'The City Login', $widget_ops);
+    $widget_ops = array('classname' => 'the_city_chipin_widget', 
+                        'description' => 'A WordPress plugin that allows users to chipin directly to The City from your WordPress website.' );
+    $this->WP_Widget('the-city-chipin-widget', 'The City Chipin', $widget_ops);
   }
   
 
   function form($instance) {
     /* Set up some default widget settings. */
 		$defaults = array( 'subdomain_key' => '', 
-                       'login_display_choice' => '');
+                       'chipin_display_choice' => '');
 
 		$instance = wp_parse_args( (array) $instance, $defaults );    
 
-    $subdomain_key = strip_tags($instance['subdomain_key']);
-    $login_display_choice = strip_tags($instance['login_display_choice']);
-    $show_remember_me = strip_tags($instance['show_remember_me']);
-    $use_placeholder_text = strip_tags($instance['use_placeholder_text']);
+    $secret_key = strip_tags($instance['secret_key']);
+    $user_token = strip_tags($instance['user_token']);
+    $chipin_display_choice = strip_tags($instance['chipin_display_choice']);
+
+    $campus_id = strip_tags($instance['campus_id']);
+    $fund_id = strip_tags($instance['fund_id']);
+    $suggested_amount = strip_tags($instance['suggested_amount']);
+    $designation = strip_tags($instance['designation']);
+
     ?>
 
-   <p>
-     <label for="<?php echo $this->get_field_id('subdomain_key'); ?>">
-       Subdomain: 
-       <input class="widefat" 
-              id="<?php echo $this->get_field_id('subdomain_key'); ?>" 
-              name="<?php echo $this->get_field_name('subdomain_key'); ?>" 
+    <p>
+      <label for="<?php echo $this->get_field_id('secret_key'); ?>">
+        City Secret Key: 
+        <input class="widefat" 
+              id="<?php echo $this->get_field_id('secret_key'); ?>" 
+              name="<?php echo $this->get_field_name('secret_key'); ?>" 
               type="text" 
-              value="<?php echo attribute_escape($subdomain_key); ?>" />
+              value="<?php echo attribute_escape($secret_key); ?>" />
       </label>
-      <i>Ex: https://[subdomain].OnTheCity.org</i>
+    </p>
+
+    <p>
+      <label for="<?php echo $this->get_field_id('user_token'); ?>">
+        City User Token: 
+        <input class="widefat" 
+              id="<?php echo $this->get_field_id('user_token'); ?>" 
+              name="<?php echo $this->get_field_name('user_token'); ?>" 
+              type="text" 
+              value="<?php echo attribute_escape($user_token); ?>" />
+      </label>
     </p>
 
 
     <?php 
       $plain_s = $inline_s = '';
-      switch($instance['login_display_choice']) {
+      switch($instance['chipin_display_choice']) {
         case 'plain':
           $plain_s = 'selected="selected"'; 
           break;
@@ -65,11 +80,11 @@ class The_City_Login_Widget extends WP_Widget {
     ?> 
 
     <p>    
-      <label for="<?php echo $this->get_field_id('login_display_choice'); ?>">
+      <label for="<?php echo $this->get_field_id('chipin_display_choice'); ?>">
         Display:        			
         <select class="widefat" 
-                id="<?php echo $this->get_field_id('login_display_choice'); ?>" 
-                name="<?php echo $this->get_field_name('login_display_choice'); ?>">
+                id="<?php echo $this->get_field_id('chipin_display_choice'); ?>" 
+                name="<?php echo $this->get_field_name('chipin_display_choice'); ?>">
             <option value="plain" <?php echo $plain_s; ?> >Plain</option>
         		<option value="inline" <?php echo $inline_s; ?> >Inline</option>
             <option value="city_style_normal" <?php echo $city_style_normal; ?> >City Style Normal</option>
@@ -78,25 +93,59 @@ class The_City_Login_Widget extends WP_Widget {
       </label>    
     </p>    
 
-    <?php $show_remember_me_checked = empty($show_remember_me) ? '' : 'checked="checked"'; ?>
-    <p>
-      <label for="<?php echo $this->get_field_id('show_remember_me'); ?>">
-        <input type="checkbox" 
-               id="<?php echo $this->get_field_id('show_remember_me'); ?>" 
-               name="<?php echo $this->get_field_name('show_remember_me'); ?>"
-               <?php echo $show_remember_me_checked ?> /> Show Remember me checkbox       
-      </label>
-    <p>
 
-    <?php $use_placeholder_text_checked = empty($use_placeholder_text) ? '' : 'checked="checked"'; ?>
+    <p>    
+      <label for="<?php echo $this->get_field_id('campus_id'); ?>">
+        Church/Campus:              
+        <select class="widefat" 
+                id="<?php echo $this->get_field_id('campus_id'); ?>" 
+                name="<?php echo $this->get_field_name('campus_id'); ?>">
+            <option value="1">Sparks</option>
+            <option value="2">Reno</option>
+        </select>
+      </label>    
+    </p>    
+
+
+    <p>    
+      <label for="<?php echo $this->get_field_id('fund_id'); ?>">
+        Fund:              
+        <select class="widefat" 
+                id="<?php echo $this->get_field_id('fund_id'); ?>" 
+                name="<?php echo $this->get_field_name('fund_id'); ?>">
+            <option value="1">Expansion 2013</option>
+            <option value="2">Expansion 2013</option>
+        </select>
+      </label>    
+    </p>        
+
+
     <p>
-      <label for="<?php echo $this->get_field_id('use_placeholder_text'); ?>">
-        <input type="checkbox" 
-               id="<?php echo $this->get_field_id('use_placeholder_text'); ?>" 
-               name="<?php echo $this->get_field_name('use_placeholder_text'); ?>"
-               <?php echo $use_placeholder_text_checked ?> /> Use placeholder text       
+      <label for="<?php echo $this->get_field_id('suggested_amount'); ?>">
+        Suggested Amount (optional): 
+        <input class="widefat" 
+              id="<?php echo $this->get_field_id('suggested_amount'); ?>" 
+              name="<?php echo $this->get_field_name('suggested_amount'); ?>" 
+              type="text" 
+              value="<?php echo attribute_escape($suggested_amount); ?>" />
       </label>
-    <p>      
+      <i>Ex: "25" for "$25"</i>
+    </p>
+
+
+    <p>
+      <label for="<?php echo $this->get_field_id('designation'); ?>">
+        Designation: 
+        <input class="widefat" 
+              id="<?php echo $this->get_field_id('designation'); ?>" 
+              name="<?php echo $this->get_field_name('designation'); ?>" 
+              type="text" 
+              value="<?php echo attribute_escape($designation); ?>" />
+      </label>
+      <i>Ex: "Chairs", Classrooms", etc.</i>
+    </p>
+
+
 
     <?php
   }
@@ -104,10 +153,15 @@ class The_City_Login_Widget extends WP_Widget {
 
   function update($new_instance, $old_instance) {
     $instance = $old_instance;
-    $instance['subdomain_key'] = strip_tags($new_instance['subdomain_key']);
-    $instance['login_display_choice'] = strip_tags($new_instance['login_display_choice']);
-    $instance['show_remember_me'] = strip_tags($new_instance['show_remember_me']);
-    $instance['use_placeholder_text'] = strip_tags($new_instance['use_placeholder_text']);
+    $instance['secret_key'] = strip_tags($new_instance['secret_key']);
+    $instance['user_token'] = strip_tags($new_instance['user_token']);
+    $instance['chipin_display_choice'] = strip_tags($new_instance['chipin_display_choice']);
+
+    $instance['campus_id'] = strip_tags($new_instance['campus_id']);
+    $instance['fund_id'] = strip_tags($new_instance['fund_id']);
+    $instance['suggested_amount'] = strip_tags($new_instance['suggested_amount']);
+    $instance['designation'] = strip_tags($new_instance['designation']);
+
     return $instance;
   }
   
@@ -115,16 +169,27 @@ class The_City_Login_Widget extends WP_Widget {
 
   function widget($args, $instance) {
     extract($args);
-    $subdomain_key = empty($instance['subdomain_key']) ? ' ' : $instance['subdomain_key'];
-    $login_display_choice = empty($instance['login_display_choice']) ? 'plain' : $instance['login_display_choice'];
-    $show_remember_me = empty($instance['show_remember_me']) ? ' ' : $instance['show_remember_me'];
-    $use_placeholder_text = empty($instance['use_placeholder_text']) ? ' ' : $instance['use_placeholder_text'];
+    $secret_key = empty($instance['secret_key']) ? ' ' : $instance['secret_key'];
+    $user_token = empty($instance['user_token']) ? ' ' : $instance['user_token'];
+    $chipin_display_choice = empty($instance['chipin_display_choice']) ? 'plain' : $instance['chipin_display_choice'];
+
+    $campus_id = empty($instance['campus_id']) ? ' ' : $instance['campus_id'];
+    $fund_id = empty($instance['fund_id']) ? ' ' : $instance['fund_id'];
+    $suggested_amount = empty($instance['suggested_amount']) ? ' ' : $instance['suggested_amount'];
+    $designation = empty($instance['designation']) ? ' ' : $instance['designation'];
+
+$title = 'Dog';
+    echo $before_widget;
+    if (!empty( $title )) {
+        echo $before_title . $title . $after_title;
+    };
+
     include dirname(__FILE__).'/widget_info.php';
     echo $after_widget;
   }
   
 }
 
-add_action('widgets_init', create_function('', 'return register_widget("The_City_Login_Widget");'));
+add_action('widgets_init', create_function('', 'return register_widget("The_City_Chipin_Widget");'));
 
 ?>
--- thecity_chipin_scripts.php
+++ thecity_chipin_scripts.php
@@ -1,30 +1,30 @@
 <?php
 
-function thecity_login_wordpress_scripts() {     
-  wp_register_script('thecity_login_js', plugins_url('/the-city-login/scripts/thecity_login.js'));   
-  wp_register_script('thecity_login_load_js', plugins_url('/the-city-login/scripts/thecity_login_load.js')); 
-  wp_register_script('formatter_city_style_js', plugins_url('/the-city-login/scripts/formatter_city_style.js')); 
+function thecity_chipin_wordpress_scripts() {     
+  wp_register_script('thecity_chipin_js', plugins_url('/the-city-chipin/scripts/thecity_chipin.js'));   
+  wp_register_script('thecity_chipin_load_js', plugins_url('/the-city-chipin/scripts/thecity_chipin_load.js')); 
+  wp_register_script('formatter_city_style_js', plugins_url('/the-city-chipin/scripts/formatter_city_style.js')); 
 
   wp_enqueue_script( array('jquery') );  
-  wp_enqueue_script('thecity_login_js');    
-  wp_enqueue_script('thecity_login_load_js');  
+  wp_enqueue_script('thecity_chipin_js');    
+  wp_enqueue_script('thecity_chipin_load_js');  
   wp_enqueue_script('formatter_city_style_js');  
 }
 
-add_action('wp_enqueue_scripts', 'thecity_login_wordpress_scripts');  
+add_action('wp_enqueue_scripts', 'thecity_chipin_wordpress_scripts');  
 
 
 
-function thecity_login_wordpress_styles() {  
-  wp_register_style('thecity_login_wordpress_style', plugins_url( '/the-city-login/scripts/thecity_login.css'));   
-  wp_register_style('thecity_appkit', plugins_url( '/the-city-login/scripts/thecity_appkit.css'));     
-  wp_register_style('formatter_city_style_css', plugins_url( '/the-city-login/scripts/formatter_city_style.css'));   
+function thecity_chipin_wordpress_styles() {  
+  wp_register_style('thecity_chipin_wordpress_style', plugins_url( '/the-city-chipin/scripts/thecity_chipin.css'));   
+  wp_register_style('thecity_appkit', plugins_url( '/the-city-chipin/scripts/thecity_appkit.css'));     
+  wp_register_style('formatter_city_style_css', plugins_url( '/the-city-chipin/scripts/formatter_city_style.css'));   
 
-  wp_enqueue_style('thecity_login_wordpress_style'); 
+  wp_enqueue_style('thecity_chipin_wordpress_style'); 
   wp_enqueue_style('thecity_appkit');  
   wp_enqueue_style('formatter_city_style_css');  
 }
 
-add_action('wp_enqueue_scripts', 'thecity_login_wordpress_styles');  
+add_action('wp_enqueue_scripts', 'thecity_chipin_wordpress_styles');  
 
 ?>
